52903644248f01fd98523c0d84c2c8f270b072f4
methimpact <methimpact@gmail.com>
2017-08-24 23:21:32 +0200

    Create 51-prothese.sh

---
+++ 51-prothese.sh
@@ -0,0 +1,13 @@
+USAGE_ratooch=''
+HELP_ratooch='
+a chroot. 
+'
+cmd_ratooch() (
+  echo "Bah ";;
+  echo "Bah c'est ";;
+  echo "Bah c'est l-histoire";;
+  echo "Bah c'est l-histoire d'un";;
+  echo "Bah c'est l-histoire d'un mort";;
+  echo "mort...";;
+  esac
+)
