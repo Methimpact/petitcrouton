fc673f098ae9604c26fedc34ecb0dbc3dbade35c
methimpact <methimpact@gmail.com>
2017-08-26 20:02:17 +0200

    Update helixir_install.sh

--- helixir_install.sh
+++ helixir_install.sh
@@ -1,2 +1,6 @@
-sudo apt-get install esl-erlang
-sudo apt-get install elixir
+PKG :
+  wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+
+APT :
+  sudo apt-get install esl-erlang
+  sudo apt-get install elixir
